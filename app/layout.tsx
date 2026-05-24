a7c12f2c6bd0dc970174678b8d121a61d2cd229c
Eshan05 <nahareshan05@gmail.com>
2026-05-24 23:50:18 +0530

    Some components and CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata, Viewport } from 'next'
+import { ThemeProvider } from 'next-themes'
+import { Geist, JetBrains_Mono } from 'next/font/google'
+import { Toaster } from 'sonner'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import './globals.css'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = JetBrains_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
@@ -20,14 +23,26 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${geistSans.variable} antialiased`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TooltipProvider>
+            <div data-vaul-drawer-wrapper='' className='overflow-x-hidden'>
+              {children}
+            </div>
+          </TooltipProvider>
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
+
---
+++ components/shared/made-by.tsx
@@ -0,0 +1,13 @@
+import { HeartIcon } from 'lucide-react'
+import Link from 'next/link'
+
+export const MadeBy = () => {
+  return (
+    <div className='flex items-center justify-center gap-2 p-2 text-xs text-muted-foreground'>
+      <HeartIcon className='w-4 h-4 text-red-400/50 animate-pulse' />
+      <span className='text-muted-foreground'>
+        Made by Eshan
+      </span>
+    </div>
+  )
+}
